6340de32305eea0e257c797fffc0ef304ae5c3c2
chengyuankuo <chengyk@rd.netease.com>
2020-05-26 21:19:45 +0800

    WIP:

---
+++ src/main/java/cn/cykuo/aop/MyService.java
@@ -0,0 +1,15 @@
+package cn.cykuo.aop;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author chengyuankuo
+ * @date 2020/05/24
+ */
+@Service
+public class MyService {
+
+    public int div(int i,int j){
+        return  i / j;
+    }
+}
